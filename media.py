630ec32048cd72e8d2419ecedf6c66d8bf30b2e2
Sattam Alharbi <sattamjh@gmail.com>
2017-07-27 15:52:57 +0300

    initial commit

---
+++ media.py
@@ -0,0 +1,15 @@
+'''
+Movie Trailer Website
+Udacity Nanodegree Project
+Author: Sattamjh
+
+'''
+
+class Movie(object):
+    '''This class provides a way to store movie related information '''
+
+    def __init__(self, title, poster_image, trailer_youtube):
+        self.title = title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+    
